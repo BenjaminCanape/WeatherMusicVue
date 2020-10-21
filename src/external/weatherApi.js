373a7251f5dd158cf66985e2cfcd5751cd7518ed
Benjamin Canape <benjamin.canape@gmail.com>
2020-10-21 21:31:35 +0200

    change protocol for weather api

--- src/external/weatherApi.js
+++ src/external/weatherApi.js
@@ -1,5 +1,5 @@
 const WEATHER_API_KEY = process.env.VUE_APP_WEATHER_API_KEY;
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 export default function getWeatherResponseFromAPI(localisation) {
     console.log(WEATHER_API_KEY);
